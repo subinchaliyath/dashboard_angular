b15458ab15a8891417b42afc5cfa952bcd78e112
Subin Chaliyath <subinchaliyath@gmail.com>
2020-08-02 20:27:18 +0530

    dashboard basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {SettingsComponent} from './components/settings/settings.component';
+import {AboutComponent} from './components/about/about.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"",component:DashboardComponent},
+  {path:"settings",component:SettingsComponent},
+  {path:"about",component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import {AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    SettingsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
